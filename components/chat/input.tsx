2469115b34ac5753321901005d84f6c6942c01fc
Thomas Marchand <thomas.marchand@tuta.io>
2023-05-06 11:34:06 +0200

    feat: add a nice loading fidget

--- components/chat/input.tsx
+++ components/chat/input.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import autosize from "autosize";
 import styles from "./chat.module.css";
+import { DotLoader } from "react-spinners";
 
 export default function Input({
   addMessage,
@@ -24,6 +25,12 @@ export default function Input({
 
       addMessage({ content: messageContent, bot: false });
       setUserInput("");
+      if (shouldScrollToBottom && messageWrapper) {
+        setTimeout(() => {
+          messageWrapper.scrollTop =
+            messageWrapper.scrollHeight - messageWrapper.clientHeight;
+        }, 0);
+      }
 
       (async () => {
         setWaiting(true);
@@ -63,39 +70,50 @@ export default function Input({
   useEffect(() => {
     const textarea = document.querySelector(`.${styles.userInput}`);
     if (textarea) {
-      autosize(textarea);
+      autosize.update(textarea);
     }
-  }, []);
+  }, [userInput]);
 
   return (
     <div className={styles.inputWrapper}>
       <textarea
-        className={styles.userInput}
-        placeholder="Type your message..."
+        className={`${styles.userInput} ${waiting ? styles.disabled : ""}`}
+        placeholder={waiting ? "Jarvis is thinking" : "Type your message..."}
         rows={1}
         value={userInput}
+        disabled={waiting}
         onChange={(e) => {
           setUserInput(e.target.value);
           autosize(e.target);
         }}
         onKeyDown={handleKeyPress}
       />
-      <svg
-        className={styles.sendButton}
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-        onClick={handleSendMessage}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-        ></path>
-      </svg>
+      {waiting ? (
+        <DotLoader color="#dee0e4" size={24} className={styles.loading} />
+      ) : (
+        <SendIcon onMsg={handleSendMessage} />
+      )}
     </div>
+  );
+}
+
+function SendIcon({ onMsg }: { onMsg: () => void }) {
+  return (
+    <svg
+      className={styles.sendButton}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      onClick={onMsg}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+      ></path>
+    </svg>
   );
 }
