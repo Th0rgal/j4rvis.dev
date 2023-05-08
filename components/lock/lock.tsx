deb5331c3884638e3667d2692dc005d0fa7c67f3
Thomas Marchand <thomas.marchand@tuta.io>
2023-05-08 19:01:21 +0200

    fix: showing password form when not needed

--- components/lock/lock.tsx
+++ components/lock/lock.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import styles from "./Lock.module.css";
 import { useCookie } from "@/hooks/useCookie"; // Adjust the import path accordingly
 
@@ -7,6 +7,13 @@ export default function Lock() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const token = useCookie("token");
+  const [hasCheckedToken, setHasCheckedToken] = useState(false);
+
+  useEffect(() => {
+    if (token !== undefined) {
+      setHasCheckedToken(true);
+    }
+  }, [token]);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -31,6 +38,10 @@ export default function Lock() {
       window.location.reload();
     }
   };
+
+  if (!hasCheckedToken) {
+    return null;
+  }
 
   return token ? null : (
     <div className={styles.overlay}>
